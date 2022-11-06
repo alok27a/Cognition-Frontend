5990e4b7154bcb120fa104ed0b663d7aef647b07
Nanduri Jayant Vishnu <nandurijayant.vishnu2020@vitstudent.ac.in>
2022-11-06 11:47:16 +0530

    added image authentication

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ import {
   toast,
   Textarea,
   useClipboard,
-  Text
+  Text,
 } from "@chakra-ui/react";
 
 import {
@@ -24,7 +24,7 @@ import {
   InputGroup,
   FormErrorMessage,
   FormHelperText,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
 import {
   Modal,
@@ -34,90 +34,160 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  InputRightElement
-} from '@chakra-ui/react'
-import { CopyIcon } from '@chakra-ui/icons'
+  InputRightElement,
+} from "@chakra-ui/react";
+import { CopyIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
 
 import Security from "../assets/security.svg";
 
 const Hero = () => {
-  const toast = useToast()
+  const toast = useToast();
 
-  const [show, setShow] = React.useState(false)
-  const [show1, setShow1] = React.useState(false)
+  const [show, setShow] = React.useState(false);
+  const [show1, setShow1] = React.useState(false);
   const [token, setToken] = React.useState(false);
-  const [password, setPassword] = React.useState("")
-  const [confirmpassword, setConfirmPassword] = React.useState("")
-  let [value, setValue] = React.useState('')
+  const [password, setPassword] = React.useState("");
+  const [confirmpassword, setConfirmPassword] = React.useState("");
+  let [value, setValue] = React.useState("");
   const { onCopy, hasCopied } = useClipboard(value);
-  const[registerLoading, setRegisterLoading] = React.useState(false)
-
+  const [registerLoading, setRegisterLoading] = React.useState(false);
+  const [image1,setImage1] = React.useState('');
+  const [image2,setImage2] = React.useState('');
+  const [image3,setImage3] = React.useState('');
+  const [image4,setImage4] = React.useState('');
   let navigate = useNavigate();
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen: isOpen, onOpen: onOpen, onClose: onClose } = useDisclosure();
+  const {
+    isOpen: isOpen2,
+    onOpen: onOpen2,
+    onClose: onClose2,
+  } = useDisclosure();
 
-  const initialRef = React.useRef(null)
-  const finalRef = React.useRef(null)
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
 
-  const handleClick = () => setShow(!show)
-  const handleClick1 = () => setShow1(!show1)
+  const handleClick = () => setShow(!show);
+  const handleClick1 = () => setShow1(!show1);
 
   let handleInputChange = (e) => {
-    let inputValue = e.target.value
-    setValue(inputValue)
-  }
+    let inputValue = e.target.value;
+    setValue(inputValue);
+  };
 
   const savePassword = async () => {
-    if (password != confirmpassword || password.length == 0 || confirmpassword.length == 0) {
+    if (
+      password != confirmpassword ||
+      password.length == 0 ||
+      confirmpassword.length == 0
+    ) {
       toast({
-        title: 'Error!',
+        title: "Error!",
         description: "Password and Re-Enter pasword not matched",
-        status: 'error',
+        status: "error",
         duration: 3000,
         isClosable: true,
-      })
-    }
-    else {
+      });
+    } else {
       // console.log(password)
-      setRegisterLoading(true)
-      let result = await fetch("https://safe-chain.vercel.app/user/create", {
+      setRegisterLoading(true);
+      let result = await fetch("http://localhost:3000/user/create", {
         method: "POST",
         body: JSON.stringify({
-          "password": password
+          password: password,
         }),
         headers: {
           "Content-Type": "application/json",
-          'Accept': 'application/json',
-          "Access-Control-Allow-Origin": "*"
-        }
-      })
-      setRegisterLoading(false)
-      let test = await result.json()
-      if(test.success){
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      setRegisterLoading(false);
+      let test = await result.json();
+      if (test.success) {
         toast({
-          title: 'Success!',
+          title: "Success!",
           description: test.message,
-          status: 'success',
+          status: "success",
           duration: 3000,
           isClosable: true,
-        })
+        });
+        onOpen2();
+
       } else {
         toast({
-          title: 'Eror!',
+          title: "Eror!",
           description: test.message,
-          status: 'error',
+          status: "error",
           duration: 3000,
           isClosable: true,
-        })
+        });
       }
-      setValue(test.token)
-      setToken(true)
-      console.log("result", test)
+      setValue(test.token);
+      setToken(true);
+      console.log("result", test);
     }
+  };
+
+  //vishnu's addition
+  const imageFileUpload = (e, a) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = () => {
+      if(a==1){
+        setImage1(reader.result);
+      }else if(a==2){
+        setImage2(reader.result);
+      }
+      else if(a==3){
+        setImage3(reader.result);
+      }else{
+        setImage4(reader.result);
+      }
+    };
+    reader.onerror = (error) => {
+      console.log("Error: ", error);
+    };
+  };
+
+  const submitImages = ()=>{
+    fetch('http://localhost:3000/images/upload',{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json",
+        "auth-token":value,
+        "password":password
+      },
+      body:JSON.stringify({image1:window.btoa(image1),image2:window.btoa(image2),image3:window.btoa(image3),image4:window.btoa(image4)})
+    }).then(response=>response.json()).then((data)=>{
+      if(data.success){
+        console.log(data);
+        toast({
+          title: "Success!",
+          description: "Image order saved.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+        setTimeout(()=>{
+          location.reload();
+        },1000);
+      }else{
+        toast({
+          title: "Error!",
+          description: "Image order already saved.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+        setTimeout(()=>{
+          location.reload();
+        },1000);
+        console.log(data);
+      }
+    })
   }
-
-
   return (
     <Container maxW="container.xl" bg="blue.50">
       <Stack direction={{ base: "column", md: "row" }} py={8}>
@@ -145,17 +215,19 @@ const Hero = () => {
                   <ModalBody pb={6}>
                     <FormControl>
                       <FormLabel>Master Password</FormLabel>
-                      <InputGroup size='md'>
+                      <InputGroup size="md">
                         <Input
                           value={password}
-                          onChange={(e) => { setPassword(e.target.value) }}
-                          pr='4.5rem'
-                          type={show ? 'text' : 'password'}
-                          placeholder='Enter password'
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
+                          pr="4.5rem"
+                          type={show ? "text" : "password"}
+                          placeholder="Enter password"
                         />
-                        <InputRightElement width='4.5rem'>
-                          <Button h='1.75rem' size='sm' onClick={handleClick}>
-                            {show ? 'Hide' : 'Show'}
+                        <InputRightElement width="4.5rem">
+                          <Button h="1.75rem" size="sm" onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
                           </Button>
                         </InputRightElement>
                       </InputGroup>
@@ -163,47 +235,102 @@ const Hero = () => {
 
                     <FormControl mt={4}>
                       <FormLabel>Re-Enter Master Password</FormLabel>
-                      <InputGroup size='md'>
+                      <InputGroup size="md">
                         <Input
                           value={confirmpassword}
-                          onChange={(e) => { setConfirmPassword(e.target.value) }}
-                          pr='4.5rem'
-                          type={show1 ? 'text' : 'password'}
-                          placeholder='Re-Enter password'
+                          onChange={(e) => {
+                            setConfirmPassword(e.target.value);
+                          }}
+                          pr="4.5rem"
+                          type={show1 ? "text" : "password"}
+                          placeholder="Re-Enter password"
                         />
-                        <InputRightElement width='4.5rem'>
-                          <Button h='1.75rem' size='sm' onClick={handleClick1}>
-                            {show1 ? 'Hide' : 'Show'}
+                        <InputRightElement width="4.5rem">
+                          <Button h="1.75rem" size="sm" onClick={handleClick1}>
+                            {show1 ? "Hide" : "Show"}
                           </Button>
                         </InputRightElement>
                       </InputGroup>
                     </FormControl>
                   </ModalBody>
-
                   <ModalFooter>
-                    <Button colorScheme='blue' mr={3} onClick={savePassword} isLoading={registerLoading} loadingText="Submitting">
+                    <Button
+                      colorScheme="blue"
+                      mr={3}
+                      onClick={savePassword}
+                      isLoading={registerLoading}
+                      loadingText="Submitting"
+                    >
                       Submit
                     </Button>
                     <Button onClick={onClose}>Cancel</Button>
                   </ModalFooter>
-                  {token && <Box
-                    value={value}
-                    onChange={handleInputChange}
-                    w='100%'
-                    p='5'
-                    color='black'
-                    bg='blue.50'
-                  >
-                    <Text fontSize='xl'>
-                      Please Keep this Encrypted Key Safely!
-                    </Text>
-                    <br></br>
-                    {value}
-                    <br></br>
-                    <Button onClick={onCopy} ml={2} leftIcon={<CopyIcon/>} colorScheme='blue'>
-                      {hasCopied ? "Copied" : "Copy"}
+                </ModalContent>
+              </Modal>
+              <Modal
+                initialFocusRef={initialRef}
+                finalFocusRef={finalRef}
+                isOpen={isOpen2}
+                onClose={onClose2}
+              >
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Choose Your Image Order</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody pb={6}>
+                    <Text m={4} ml={0}><b>Choose Image 1</b></Text>
+                    <Input isInvalid={!image1} p={1} type="file" mb={4} onChange={(e)=>{imageFileUpload(e,1)}}/>
+                    <Box m={4}><img src={image1} title="image1" alt=""/></Box>
+                    <Text m={4} ml={0}><b>Choose Image 2</b></Text>
+                    <Input isInvalid={!image2} p={1} type="file" mb={4} onChange={(e)=>{imageFileUpload(e,2)}}/>
+                    <Box m={4}><img src={image2} title="image1" alt=""/></Box>
+                    <Text m={4} ml={0}><b>Choose Image 3</b></Text>
+                    <Input isInvalid={!image3} p={1} type="file" mb={4} onChange={(e)=>{imageFileUpload(e,3)}}/>
+                    <Box m={4}><img src={image3} title="image1" alt=""/></Box>
+                    <Text m={4} ml={0}><b>Choose Image 4</b></Text>
+                    <Input isInvalid={!image4} p={1} type="file" mb={4} onChange={(e)=>{imageFileUpload(e,4)}}/>
+                    <Box m={4}><img src={image4} title="image1" alt=""/></Box>
+                  {(!image1||!image2||!image3||!image4)&&<Text color="red">Please select four images **</Text>}
+                  </ModalBody>
+                  <ModalFooter>
+                    
+                    <Button
+                      colorScheme="blue"
+                      mr={3}
+                      onClick={submitImages}
+                      isLoading={registerLoading}
+                      loadingText="Submitting"
+                      disabled={!image1||!image2||!image3||!image4}
+                    >
+                      Submit
                     </Button>
-                  </Box>}
+                    <Button onClick={onClose2}>Cancel</Button>
+                  </ModalFooter>
+                  {token && (
+                    <Box
+                      value={value}
+                      onChange={handleInputChange}
+                      w="100%"
+                      p="5"
+                      color="black"
+                      bg="blue.50"
+                    >
+                      <Text fontSize="xl">
+                        Please Keep this Encrypted Key Safely!
+                      </Text>
+                      <br></br>
+                      {value}
+                      <br></br>
+                      <Button
+                        onClick={onCopy}
+                        ml={2}
+                        leftIcon={<CopyIcon />}
+                        colorScheme="blue"
+                      >
+                        {hasCopied ? "Copied" : "Copy"}
+                      </Button>
+                    </Box>
+                  )}
                 </ModalContent>
               </Modal>
             </Stack>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Box,
   Flex,
@@ -14,7 +14,9 @@ import {
   FormLabel,
   useToast,
   Input,
-  InputGroup
+  InputGroup,
+  Text,
+  Spinner,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { NavLink, useNavigate } from "react-router-dom";
@@ -26,19 +28,16 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  InputRightElement
-} from '@chakra-ui/react'
-import logo from '../assets/cover.png'
-
-
-
+  InputRightElement,
+} from "@chakra-ui/react";
+import logo from "../assets/cover.png";
 const Links = ["Home"];
 const LinkURLS = ["/", "/about", "/contact"];
 
 const ExternalLinks = ["About", "Github"];
 const ExternalLinkUrls = [
-  "https://devpost.com/software/safe-chain",//Devpost Link
-  "https://github.com/alok27a/Cognition-Frontend",// Github link 
+  "", //Devpost Link
+  "https://github.com/alok27a/Cognition-Frontend", // Github link
 ];
 
 const externalLink = ({ link, index }) => {
@@ -65,71 +64,155 @@ const MyNavLink = ({ link, index }) => {
 };
 
 const Navbar = () => {
-  const toast = useToast()
-
+  const toast = useToast();
+  const [imageOrder, setImageOrder] = React.useState();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isOpen2,
+    onOpen: onOpen2,
+    onClose: onClose2,
+  } = useDisclosure();
   let navigate = useNavigate();
 
-  const initialRef = React.useRef(null)
-  const finalRef = React.useRef(null)
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
 
+  const [show, setShow] = React.useState(false);
+  const [show1, setShow1] = React.useState(false);
+  const [password, setPassword] = React.useState("");
+  const [token, setToken] = React.useState("");
+  const [loginLoading, setLoginLoading] = React.useState(false);
+  const [image1, setImage1] = React.useState("");
+  const [image2, setImage2] = React.useState("");
+  const [image3, setImage3] = React.useState("");
+  const [image4, setImage4] = React.useState("");
 
-
-  const [show, setShow] = React.useState(false)
-  const [show1, setShow1] = React.useState(false)
-  const [password, setPassword] = React.useState("")
-  const [token, setToken] = React.useState("")
-  const [loginLoading, setLoginLoading] = React.useState(false)
-
-  const handleClick = () => setShow(!show)
-  const handleClick1 = () => setShow1(!show1)
+  const [index1, setIndex1] = React.useState();
+  const [index2, setIndex2] = React.useState();
+  const [index3, setIndex3] = React.useState();
+  const [index4, setIndex4] = React.useState();
+  const [imageLoading,setImageLoading] = React.useState(false);
+  const [imageSending,setImageSending] = React.useState(false);
+  const [finalImageOrder, setFinalImageOrder] = React.useState([]);
+  const handleClick = () => setShow(!show);
+  const handleClick1 = () => setShow1(!show1);
 
   const signinClicked = async () => {
     if (password.length == 0 || token.length == 0) {
       toast({
-        title: 'Error!',
+        title: "Error!",
         description: "No value Entered",
-        status: 'error',
+        status: "error",
         duration: 9000,
         isClosable: true,
-      })
-    }
-    else {
+      });
+    } else {
       // console.log(password)
-      setLoginLoading(true)
-      let result = await fetch("https://safe-chain.vercel.app/user/login", {
+      setLoginLoading(true);
+      let result = await fetch("http://localhost:3000/user/login", {
         method: "POST",
         body: JSON.stringify({
-          "password": password,
-          "token": token
+          password: password,
+          token: token,
         }),
         headers: {
           "Content-Type": "application/json",
-          'Accept': 'application/json',
-          "Access-Control-Allow-Origin": "*"
-        }
-      })
-      setLoginLoading(false)
-      let test = await result.json()
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      setLoginLoading(false);
+      let test = await result.json();
       if (test.success) {
-        console.log("result", test)
-        sessionStorage.setItem("secretKey", test.token)
-        navigate("/dashboard");
-      }
-      else {
+        console.log("result", test);
+        sessionStorage.setItem("secretKey", test.token);
+        sessionStorage.setItem("password", password);
+        onOpen2();
+        setImageLoading(true);
+        fetch("http://localhost:3000/images/all", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": sessionStorage.getItem("secretKey"),
+            password: sessionStorage.getItem("password"),
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setImageLoading(false);
+            if (data.success) {
+              setImage1(window.atob(data.data.image1));
+              setImage2(window.atob(data.data.image2));
+              setImage3(window.atob(data.data.image3));
+              setImage4(window.atob(data.data.image4));
+            }
+          });
+      } else {
         toast({
-          title: 'Error!',
+          title: "Error!",
           description: "Incorrect Password or Secret Key",
-          status: 'error',
+          status: "error",
           duration: 9000,
           isClosable: true,
-        })
+        });
       }
-
     }
-  }
+  };
 
-
+  const setIndex = (indexVal) => {
+    const temp = [...finalImageOrder];
+    console.log(finalImageOrder);
+    if (finalImageOrder.includes(indexVal)) {
+      temp.splice(temp.indexOf(indexVal), 1);
+      setFinalImageOrder(temp);
+    } else {
+      temp.push(indexVal);
+      setFinalImageOrder(temp);
+    }
+  };
+  const imagesSend = () => {
+    setImageSending(true);
+    const temp = [image1, image2, image3, image4];
+    console.log(finalImageOrder);
+    console.log(temp);
+    console.log(
+      JSON.stringify({
+        image1:temp[finalImageOrder[0]-1],
+        image2: temp[finalImageOrder[1]-1],
+        image3: temp[finalImageOrder[2]-1],
+        image4: temp[finalImageOrder[3]-1],
+      })
+    );
+    fetch("http://localhost:3000/images/verify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": sessionStorage.getItem("secretKey"),
+        "password": sessionStorage.getItem("password"),
+      },
+      body: JSON.stringify({
+        image1: window.btoa(temp[finalImageOrder[0]-1]),
+        image2: window.btoa(temp[finalImageOrder[1]-1]),
+        image3: window.btoa(temp[finalImageOrder[2]-1]),
+        image4: window.btoa(temp[finalImageOrder[3]-1]),
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setImageSending(false);
+        if (data.success) {
+          navigate("/dashboard");
+        } else {
+          toast({
+            title: "Error!",
+            description: "Image Order Does not Match",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+        }
+      });
+  };
   return (
     <Box px={4}>
       <Container maxW="container.xl" py={4}>
@@ -148,7 +231,7 @@ const Navbar = () => {
             mx={{ base: "auto", md: 0 }}
           >
             <HStack>
-              <img src={logo} width='40%'/>
+              <img src={logo} width="40%" />
             </HStack>
             <HStack spacing={8} display={{ base: "none", md: "flex" }}>
               {Links.map((link, index) => (
@@ -157,7 +240,7 @@ const Navbar = () => {
               {ExternalLinks.map((link, index) =>
                 externalLink({ link, index })
               )}
-              <Button colorScheme="blue" p={4} onClick={onOpen} >
+              <Button colorScheme="blue" p={4} onClick={onOpen}>
                 Sign In
               </Button>
               <Modal
@@ -173,17 +256,19 @@ const Navbar = () => {
                   <ModalBody pb={6}>
                     <FormControl>
                       <FormLabel>Master Password</FormLabel>
-                      <InputGroup size='md'>
+                      <InputGroup size="md">
                         <Input
                           ref={initialRef}
-                          placeholder='Enter Password Here'
-                          type={show ? 'text' : 'password'}
+                          placeholder="Enter Password Here"
+                          type={show ? "text" : "password"}
                           value={password}
-                          onChange={(e) => { setPassword(e.target.value) }}
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
                         />
-                        <InputRightElement width='4.5rem'>
-                          <Button h='1.75rem' size='sm' onClick={handleClick}>
-                            {show ? 'Hide' : 'Show'}
+                        <InputRightElement width="4.5rem">
+                          <Button h="1.75rem" size="sm" onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
                           </Button>
                         </InputRightElement>
                       </InputGroup>
@@ -192,18 +277,88 @@ const Navbar = () => {
                     <FormControl mt={4}>
                       <FormLabel> Enter Secret Key</FormLabel>
                       <Input
-                        placeholder='Enter Here'
+                        placeholder="Enter Here"
                         value={token}
-                        onChange={(e) => { setToken(e.target.value) }}
+                        onChange={(e) => {
+                          setToken(e.target.value);
+                        }}
                       />
                     </FormControl>
                   </ModalBody>
 
                   <ModalFooter>
-                    <Button colorScheme='blue' mr={3} onClick={signinClicked} isLoading={loginLoading} loadingText="Signing In">
+                    <Button
+                      colorScheme="blue"
+                      mr={3}
+                      onClick={signinClicked}
+                      isLoading={loginLoading}
+                      loadingText="Signing In"
+                    >
                       Sign In
                     </Button>
                     <Button onClick={onClose}>Cancel</Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+
+              <Modal
+                initialFocusRef={initialRef}
+                finalFocusRef={finalRef}
+                isOpen={isOpen2}
+                onClose={onClose2}
+                size="xl"
+              >
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Chose the correct image order</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody w="100%" pb={6}>
+                    <Text>Order the images: </Text>
+                    <Flex style={{ flexWrap: "wrap", flex: 1 }} gap={4}>
+                      <Box  p={4}style={{ position: "relative" }}>
+                        {imageLoading && <Spinner/>}
+                        {!imageLoading && <img src={image1} onClick={() => setIndex("1")} />}
+                        <Text style={{ position: "absolute" }}>
+                          {finalImageOrder.indexOf("1") + 1}
+                        </Text>
+                      </Box>
+                      <Box p={4}>
+                      {imageLoading && <Spinner/>}
+                        <img src={image2} onClick={() => setIndex("2")} />
+                        <Text>{finalImageOrder.indexOf("2") + 1}</Text>
+                      </Box>
+                      <Box p={4}>
+                      {imageLoading && <Spinner/>}
+                        <img src={image3} onClick={() => setIndex("3")} />
+                        <Text>{finalImageOrder.indexOf("3") + 1}</Text>
+                      </Box>
+                      <Box p={4}>
+                      {imageLoading && <Spinner/>}
+                        <img src={image4} onClick={() => setIndex("4")} />
+                        <Text>{finalImageOrder.indexOf("4") + 1}</Text>
+                      </Box>
+                    </Flex>
+                  </ModalBody>
+
+                  <ModalFooter>
+                    <Button
+                      colorScheme="blue"
+                      mr={3}
+                      onClick={imagesSend}
+                      isLoading={imageSending}
+                      loadingText="Sending Images"
+                    >
+                      Submit Images
+                    </Button>
+                    <Button
+                      mr={3}
+                      onClick={() => {
+                        setFinalImageOrder([]);
+                      }}
+                    >
+                      Clear Order
+                    </Button>
+                    <Button onClick={onClose2}>Cancel</Button>
                   </ModalFooter>
                 </ModalContent>
               </Modal>
